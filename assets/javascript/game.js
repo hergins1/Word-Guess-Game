bac5758d7ff5d6f25e0bbe42a7ffd25155670482
Erik Hergins <hergins1@hotmail.com>
2019-03-10 22:04:18 -0500

    underscores

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,9 +1,11 @@
 var answers = ["Chiefs", "Chargers", "Broncos", "Raiders"];
 var userChoices = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z",];
+var randomWord = answers[Math.floor(Math.random() * answers.length)];
 
 var wins = 0;
 var guessesRemaing = 10;
-var answersIndex = 0;
+var answersIndex = [];
+var s;
 
 var winsText = document.getElementById("wins-text");
 var guessesRemainingText = document.getElementById("guesses-remaining");
@@ -14,30 +16,40 @@ var directionsText = document.getElementById("directions-text");
 document.onkeyup = function(event){
     directionsText.textContent = " "
 
-   function renderCurrentWord() {
-    if (answersIndex <= (answers.length - 1)) {
-        document.querySelector("#current-word").innerHTML = answers[answersIndex];
+   function startUp(){
+       for (var i = 0; i< randomWord.length; i++){
+           answersIndex[i] = "_";
+       }
+       s = answersIndex.join(" ");
+       currentWordText.textContent = s;
     }
-    else if (guessesRemaing >= 0) {
-
+    
+    function letter(){
+        var letter = document.getSelection(userChoices).value;
+        
+        if (letter.length > 0){
+            for (var i = 0; i < randomWord.length; i++){
+                if (randomWord[i] === letter){
+                    answersIndex[i] = letter;
+                    
+                }
+            }
+        }
+        startUp();
+        letter();
     }
-    else if (guessesRemaing === 0){
-       document.querySelector("#current-word").innerHTML = "Game Over";
-    }
-     else {
-         document.querySelector("#current-word").innerHTML = "You Win";
-    }
-   }
-
-   currentWordText.textContent = answersIndex.length.innerHTML = "_";
     
     
     
 
 
-
-   renderCurrentWord(); 
+    guessesRemaing--;
     answersIndex++;
     winsText.textContent = "Wins: " + wins;
     guessesRemainingText.textContent = "Guesses Remaining: " + guessesRemaing;
+
+    if(guessesRemaing === 0){
+        document.querySelector("#current-word").innerHTML = "Game Over";
+    }
+
     }
